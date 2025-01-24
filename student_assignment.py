b012e6de8eaf91a3e2e0a43cd39c7fb13a5846b4
jerry10_chang <jerry10_chang@asus.com>
2025-01-24 16:15:28 +0800

    test1
    test1

--- student_assignment.py
+++ student_assignment.py
@@ -11,14 +11,16 @@ def hw02_1(q1_pdf):
     docs = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=10, chunk_overlap=0)
 
-    for idx, doc in enumerate(docs, start=1):
-        chunks = text_splitter.split_text(doc.page_content) # doc.metadata['source'], doc.metadata['page']
-        last_chunk = {
-            "file_name": q1_pdf,
-            "page_number": idx,
-            "content": chunks[-1]  # 取得當前頁面的最後一個 chunk
-        }
-    return last_chunk["content"]
+    # for idx, doc in enumerate(docs, start=1):
+    #     chunks = text_splitter.split_text(doc.page_content) # doc.metadata['source'], doc.metadata['page']        
+    #     print(len(chunks))
+    # print(type(chunks[-1]))
+    # return chunks[-1]
+    split_docs = text_splitter.split_documents(docs)
+    # for idx, doc in enumerate(split_docs):
+    #     print(f"Document {idx + 1}:\n{doc.page_content}\n")
+
+    return split_docs[-1]
 
 def hw02_2(q2_pdf):
     pass
