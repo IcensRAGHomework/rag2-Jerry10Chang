55a0760b75f26bbd15ca2ac788d57e36891c02fb
jerry10_chang <jerry10_chang@asus.com>
2025-02-03 15:59:10 +0800

    test2
    test2

--- student_assignment.py
+++ student_assignment.py
@@ -1,6 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
+import re
 
 q1_pdf = "OpenSourceLicenses.pdf"
 q2_pdf = "勞動基準法.pdf"
@@ -9,20 +10,20 @@ q2_pdf = "勞動基準法.pdf"
 def hw02_1(q1_pdf):
     loader = PyPDFLoader(q1_pdf)
     docs = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=10, chunk_overlap=0)
-
-    # for idx, doc in enumerate(docs, start=1):
-    #     chunks = text_splitter.split_text(doc.page_content) # doc.metadata['source'], doc.metadata['page']        
-    #     print(len(chunks))
-    # print(type(chunks[-1]))
-    # return chunks[-1]
+    text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
     split_docs = text_splitter.split_documents(docs)
-    # for idx, doc in enumerate(split_docs):
-    #     print(f"Document {idx + 1}:\n{doc.page_content}\n")
-
     return split_docs[-1]
 
 def hw02_2(q2_pdf):
-    pass
+    loader = PyPDFLoader(q2_pdf)
+    docs = loader.load()
+    full_text = "\n".join(doc.page_content for doc in docs)
 
-print(hw02_1(q1_pdf))
+    text_splitter = RecursiveCharacterTextSplitter(chunk_overlap=0, chunk_size=20, separators=[r'(?m)(?=^\s*第\s.*(?:條|章).*)'], is_separator_regex=True)
+    split_docs = text_splitter.split_text(full_text)
+
+    # for idx, doc in enumerate(split_docs):
+    #     print(f"------------------\n{doc}\n------------------")
+    return len(split_docs)
+
+print(hw02_2(q2_pdf))
